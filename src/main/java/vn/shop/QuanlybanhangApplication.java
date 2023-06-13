31933e9dc8e83bcdd55df308df59c7ace1badcce
Giang Pham <giangphamsd@gmail.com>
2023-06-13 10:08:52 +0700

    Edit main.java file

--- src/main/java/vn/shop/QuanlybanhangApplication.java
+++ src/main/java/vn/shop/QuanlybanhangApplication.java
@@ -5,15 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class QuanlybanhangApplication {
-
-
-
-
     public static void main(String[] args) {
         SpringApplication.run(QuanlybanhangApplication.class, args);
-
     }
-
-
-
 }
